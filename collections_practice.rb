31b58b9709af6d31b336eb6109a51ae5ca41c763
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-03 02:30:09 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,12 @@
+def sort_array_asc(array)
+  array.sort 
+  end 
+  
+def sort_array_desc(integers)
+  integers.sort {|a, b| b <=> a}
+end
+
+def sort_array_char_count(strings)
+  strings.sort {|left, right| left.length <=> right.length}
+end
 
