e38505cab459c6be74f897c660db44f9facef5fc
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-06-03 03:23:12 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -31,7 +31,6 @@ def kesha_maker(array)
 end
 
 def find_a(array)
-  array = ["apple","orange", "pear", "avis", "arlo", "ascot"]
-  array.include?("a")
-    
-end 
+  array.select{|string| string.start_with?("a")}
+end
+
